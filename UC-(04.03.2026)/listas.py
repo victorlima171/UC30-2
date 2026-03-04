56afd5555eac446c2bdcc68ed28a12e537e5d2f0
lima <victorlimac023@gmail.com>
2026-03-04 08:34:22 -0300

    Add files via upload

---
+++ UC-(04.03.2026)/listas.py
@@ -0,0 +1,32 @@
+#Listas
+
+nome1 = "ANA"
+nome2 = "BRUNO"
+nome3 = "CARLOS"
+
+
+nomes = ["Arthur", "João", "Maria"]
+print(nomes)
+
+dados = ["Gabriel", 0, 1.60, True]
+
+print(dados)
+print(type(dados))
+print(type(dados[0]))
+print(type(dados[1]))
+print(type(dados[2]))
+print(type(dados[3]))
+
+alunos = ["Adriano", "Barbara", "Camila"]
+
+print("Primeiro", alunos[0])
+print("Primeiro", alunos[1])
+print("Primeiro", len(alunos))
+print("Primeiro", alunos[2])
+print("Primeiro", alunos[len(alunos) - 1])
+
+nomes = ["Ana", "Bruno", "Caio"]
+print("Original ", nomes)
+nomes.append("Daniel")
+print("Atualizado: ", nomes)
+
